b89f844ad4bba4b2db8137b6d6bfdd0974c49db5
“SIeep” <Brandon_n_evans@live.com>
2019-02-24 12:19:37 -0600

    add random fortune messages and random numbers

--- fortune_app/app/controllers/api/fortune_controller_controller.rb
+++ fortune_app/app/controllers/api/fortune_controller_controller.rb
@@ -1,5 +1,7 @@
 class Api::FortuneControllerController < ApplicationController
   def fortune_method
-    render json: {message: 'just testing'}
+    @fortune_messages = ["You will die in 7 days", "You've been blessed by the Ruby gods. Good luck is coming your way", "You will probably not be rich ever, sorry. Bad fortune is bad."]
+    @random_numbers = (rand 0..59)
+    render 'fortune_message.json.jbuilder'
   end
 end
---
+++ fortune_app/app/views/api/fortune_controller/fortune_message.json.jbuilder
@@ -0,0 +1,2 @@
+json.message @fortune_messages[rand(0..2)]
+json.random_number @random_numbers
